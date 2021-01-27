76f21a2e9358f2348dfd839ffe8f81285cff69b7
madisonlilly22 <mal0042@mix.wvu.edu>
2021-01-27 18:25:01 -0500

    <Madison> Task 01 upload b

--- Task_01/task01.R
+++ Task_01/task01.R
@@ -1,2 +1,21 @@
 1
  # test
+ library( swirl )
+ swirl ()
+ Madison
+ 1
+ 5+7
+x <- 5 + 7
+y <- x - 3
+z <- c(1.1,9,3.14)
+?c
+z
+c(z,555,z)
+z*2+100
+my_sqrt <- sqrt(z-1)
+2
+my_sqrt
+my_div <- z/my_sqrt
+my_div
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
