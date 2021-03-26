b1c4d19a16d08dd442ace90613d01e9757d4c4ac
madisonlilly22 <mal0042@mix.wvu.edu>
2021-03-26 10:35:22 -0400

    Task_08

--- Project/Project/Code/code (Autosaved).R
+++
@@ -1,3 +0,0 @@
-setwd('~/Desktop/Evolution/Tasks/Project/Project/Code')
-Data <- read.csv('~/Desktop/Evolution/Tasks/Project/Project/Data/Mexican cavefish data.csv', stringsAsFactors=F)
-write.csv(Data, 'rawdata.csv', quote=F)
